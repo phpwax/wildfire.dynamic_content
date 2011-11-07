a854c6b69d33660cfcf9e8e81a2955f36d97e5e0
charlesmarshall <cm56marshall@googlemail.com>
2011-11-07 10:45:24 +0000

    dynamic model with events

---
+++ lib/model/WildfireDynamicContent.php
@@ -0,0 +1,29 @@
+<?
+class WildfireDynamicContent extends WildfireContent{
+  
+  public $table = "wildfire_content";
+  
+  public function setup(){
+    parent::setup();
+    WaxEvent::run("wildfire_dynamic_content.setup", $this);
+  }
+  
+  
+  public function before_save(){
+    parent::before_save();
+    WaxEvent::run("wildfire_dynamic_content.before_save", $this);
+  }
+  public function before_insert(){
+    parent::before_insert();
+    WaxEvent::run("wildfire_dynamic_content.before_insert", $this);
+  }
+  public function after_save(){
+    parent::after_save();
+    WaxEvent::run("wildfire_dynamic_content.after_save", $this);
+  }
+  public function after_insert(){
+    parent::after_insert();
+    WaxEvent::run("wildfire_dynamic_content.after_insert", $this);
+  }
+}
+?>
