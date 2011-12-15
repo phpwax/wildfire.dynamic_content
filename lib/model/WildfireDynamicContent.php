7fe1d51fdbf65d4831902c8a8b20439be0c619ef
charles marshall <charles@oneblackbear.com>
2011-12-15 12:41:48 +0000

    make that a dynamic class name

--- lib/model/WildfireDynamicContent.php
+++ lib/model/WildfireDynamicContent.php
@@ -5,30 +5,30 @@ class WildfireDynamicContent extends WildfireContent{
   
   public function setup(){
     parent::setup();
-    WaxEvent::run("wildfire_dynamic_content.setup", $this);
+    WaxEvent::run(get_class($this).".setup", $this);
   }
   
   public function scope($scope){
     $this->asked_for_scope = $scope;
-    WaxEvent::run("wildfire_dynamic_content.scope", $this);
+    WaxEvent::run(get_class($this).".scope", $this);
     return parent::scope($scope);
   }
   
   public function before_save(){
     parent::before_save();
-    WaxEvent::run("wildfire_dynamic_content.before_save", $this);
+    WaxEvent::run(get_class($this).".before_save", $this);
   }
   public function before_insert(){
     parent::before_insert();
-    WaxEvent::run("wildfire_dynamic_content.before_insert", $this);
+    WaxEvent::run(get_class($this).".before_insert", $this);
   }
   public function after_save(){
     parent::after_save();
-    WaxEvent::run("wildfire_dynamic_content.after_save", $this);
+    WaxEvent::run(get_class($this).".after_save", $this);
   }
   public function after_insert(){
     parent::after_insert();
-    WaxEvent::run("wildfire_dynamic_content.after_insert", $this);
+    WaxEvent::run(get_class($this).".after_insert", $this);
   }
 }
 ?>
